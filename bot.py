a7956d954d1706cbfca75c063da4422c9c138b19
manilawestern-debug <manilawestern@gmail.com>
2026-04-07 08:35:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,23 @@
-import asyncio
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+from telegram.ext import Updater, MessageHandler, Filters
 
 TOKEN = "8749296190:AAHHCUQChGuT_HGr9hN1IlStu0zkX96-MpE"
 
-async def delete_service(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def delete_service(update, context):
     msg = update.message
 
     if msg.new_chat_members or msg.left_chat_member:
         try:
-            await msg.delete()
+            context.bot.delete_message(
+                chat_id=msg.chat_id,
+                message_id=msg.message_id
+            )
         except:
             pass
 
-async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
 
-    app.add_handler(MessageHandler(filters.StatusUpdate.ALL, delete_service))
+dp.add_handler(MessageHandler(Filters.status_update, delete_service))
 
-    await app.run_polling()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+updater.start_polling()
+updater.idle()
