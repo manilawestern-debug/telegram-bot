4c1840b7e6a186206d8ff1d032246d57a158f5c4
manilawestern-debug <manilawestern@gmail.com>
2026-04-07 08:17:43 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,8 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters, ChatJoinRequestHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
-import os
-print("TOKEN:", os.getenv("8749296190:AAHHCUQChGuT_HGr9hN1IlStu0zkX96-MpE"))
+TOKEN = "8749296190:AAHHCUQChGuT_HGr9hN1IlStu0zkX96-MpE"
 
-# ✅ Auto approve join requests
-async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.chat_join_request.approve()
-
-# ✅ Delete join + leave messages
 async def delete_service(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message
 
@@ -20,7 +14,6 @@ async def delete_service(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(ChatJoinRequestHandler(approve))
 app.add_handler(MessageHandler(filters.StatusUpdate.ALL, delete_service))
 
 app.run_polling()
